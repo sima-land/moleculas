4d5482fed5cd999bdefed4489d5dc7d67fcd2102
Tanya <tarasova@ekb.sima-land.ru>
2020-09-04 10:14:19 +0500

    #18 Исправить проблему при загрузке фото в мобильной версии рекомендаций

--- src/mobile/components/recommendation-slider/index.jsx
+++ src/mobile/components/recommendation-slider/index.jsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import TouchSlider from '@dev-dep/ui-nucleons/touch-slider';
-import MobileLayout from '@dev-dep/ui-nucleons/layout/mobile-layout';
-import RecommendedItem from './recommended-item';
+import React, { useEffect, useRef } from 'react';
 import { isNonEmptyArray } from '../../../common/helpers/is-non-empty';
 import withInViewportObserver from '@dev-dep/ui-nucleons/with-in-viewport-observer';
-import classes from './recommendation-slider.scss';
-import classnames from 'classnames/bind';
 import isFunction from 'lodash/isFunction';
-import Text from '@dev-dep/ui-nucleons/text';
-
-const cx = classnames.bind(classes);
+import RecommendationWrapper from './recommendation-wrapper';
 
 /**
  * Компонент мобильного слайдера рекомендованных товаров.
@@ -20,7 +13,6 @@ const cx = classnames.bind(classes);
  * @param {Function} props.onInViewport Функция, вызываемая при попадании компонента в поле видимости.
  * @param {Function} props.onRequest Функция, инициализирующая загрузку рекомендаций.
  * @param {Function} props.addObserve Функция подписки на Intersection Observer.
- * @param {Function} props.addObserveWithMargin
  * Функция подписки на Intersection Observer с увеличенным пределом попадания в область видимости.
  * @return {ReactElement} Компонент карусели рекомендаций.
  */
@@ -31,72 +23,29 @@ export const RecommendationSlider = ({
   onInViewport,
   onRequest,
   addObserve,
-  addObserveWithMargin,
 }) => {
-  const sliderRef = useRef(null);
   const emptyRef = useRef(null);
-  const [canLoadImages, setCanLoadImages] = useState(false);
-  const [needLoadImages, setNeedLoadImages] = useState(false);
-
-  // Отправляем статистку в ОКО, когда компонент попадает в зону видимости
-  useEffect(() => {
-    isFunction(addObserve)
-    && isFunction(onInViewport)
-    && sliderRef.current
-    && addObserve(sliderRef.current, onInViewport);
-  }, [sliderRef.current]);
 
   // Инициируем загрузку данных, когда компонент почти попал в зону видимости
   useEffect(() => {
-    isFunction(addObserveWithMargin)
+    isFunction(addObserve)
     && isFunction(onRequest)
     && emptyRef.current
-    && addObserveWithMargin(emptyRef.current, onRequest);
+    && addObserve(emptyRef.current, onRequest);
   }, [emptyRef.current]);
 
-  // Устанавливаем возможность начала загрузки изображений, когда компонент почти попал в зону видимости
-  useEffect(() => {
-    isFunction(addObserveWithMargin)
-    && sliderRef.current
-    && addObserveWithMargin(sliderRef.current, () => setCanLoadImages(true));
-  }, [sliderRef.current]);
-
   return isNonEmptyArray(items) ? (
-    <div className={cx('wrapper')} ref={sliderRef}>
-      <MobileLayout>
-        <Text
-          size={20}
-          weight={600}
-          element='h4'
-          children={title}
-          lineHeight={28}
-        />
-      </MobileLayout>
-      <TouchSlider
-        layoutProps={{
-          containerProps: {
-            onTouchStart: () => setNeedLoadImages(true),
-          },
-        }}
-      >
-        {items.map((item, index) => (
-          <RecommendedItem
-            key={index}
-            onItemClick={() => isFunction(onItemClick) && onItemClick(item)}
-            needLoadImage={needLoadImages}
-            canLoadImage={canLoadImages}
-            {...item}
-          />
-        ))}
-      </TouchSlider>
-    </div>
+    <RecommendationWrapper
+      title={title}
+      items={items}
+      onInViewport={onInViewport}
+      onItemClick={onItemClick}
+    />
   ) : (
     <div ref={emptyRef} />
   );
 };
 
-export default withInViewportObserver(
-  withInViewportObserver(RecommendationSlider), {
-    rootMargin: '200px 0px 200px 0px',
-  }, 'addObserveWithMargin'
-);
+export default withInViewportObserver(RecommendationSlider, {
+  rootMargin: '200px 0px 200px 0px',
+});
