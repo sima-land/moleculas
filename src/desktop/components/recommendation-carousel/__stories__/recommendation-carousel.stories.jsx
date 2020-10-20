6de22cc37b6fa205d1c8ee8a096aa8e6cf77ca0c
rotanov_ps <rotanov_ps@ekb.sima-land.ru>
2020-10-20 11:47:41 +0500

    #29 Дополнить документацию компонента RecommendationCarousel - Добавил propTypes компоненту; - Доработал документацию;

--- src/desktop/components/recommendation-carousel/__stories__/recommendation-carousel.stories.jsx
+++ src/desktop/components/recommendation-carousel/__stories__/recommendation-carousel.stories.jsx
@@ -1,52 +1,14 @@
 import React from 'react';
-import RecommendationCarousel from '../index';
-import { carouselItems } from './carousel-items';
-import RecommendationCarouselButton from '../recommendation-carousel-button';
-import RecommendedItem from '../recommended-item';
 
-const Template = args => <RecommendationCarousel {...args} />;
+// Импортируем чистый компонент без HOC, чтобы корректно отображалось имя компонента.
+import { RecommendationCarousel } from '../index';
+import Box from '@dev-dep/ui-nucleons/box';
+import { carouselItems } from './carousel-items';
+
+const Template = args => <Box paddingX={12}><RecommendationCarousel {...args} /></Box>;
 
 export const Primary = Template.bind({});
 Primary.args = {
   title: 'Рекомендованные товары',
   items: carouselItems,
-};
-
-export default {
-  title: 'desktop/Recommendation carousel',
-  component: RecommendationCarousel,
-
-  // Дочерние компоненты нужны лишь для документации, не влияют на историю основного компонента.
-  subcomponents: { RecommendationCarouselButton, RecommendedItem },
-  decorators: [Story => <div style={{ width: '80%', padding: '0 40px' }}><Story /></div>],
-  argTypes: {
-    title: {
-      description: 'Заголовок секции',
-    },
-    items: {
-      description: 'Массив с рекомендованными товарами',
-      control: {
-        type: null,
-      },
-    },
-    onItemClick: {
-      description: 'Обработчик клика по товару',
-      action: 'clicked',
-    },
-    onInViewport: {
-      description: 'Функция, вызываемая при попадании компонента в поле видимости',
-      action: 'inViewport',
-    },
-    onRequest: {
-      description: 'Функция, инициализирующая загрузку рекомендаций',
-      action: 'request',
-    },
-  },
-  parameters: {
-    docs: {
-      description: {
-        component: 'Компонент карусели товаров-рекомендаций',
-      },
-    },
-  },
 };
