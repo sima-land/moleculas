e74c6f7e96b967d2eaf7ea1441c15cb33c239a5f
Швец Кирилл Сергеевич <shvets_ks@sima-land.ru>
2020-12-24 12:36:44 +0500

    fix(Card): Доработан компонент Card

--- src/common/components/card/__test__/index.test.jsx
+++ src/common/components/card/__test__/index.test.jsx
@@ -12,21 +12,27 @@ describe('<Card />', () => {
   });
 
   it('should handle props', () => {
+    const onClick = jest.fn();
     const onClose = jest.fn();
     const wrapper = mount(
       <Card
         title='Заголовок'
         withCloseButton
+        onClick={onClick}
         onClose={onClose}
         footer='Foo Bar'
-        childrenProps={{ className: 'test-class' }}
+        contentProps={{ className: 'test-class' }}
+        data-testid='component:card'
       >
         Lorem, ipsum dolor sit amet consectetur adipisicing elit.
       </Card>
     );
 
     expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('.test-class')).toHaveLength(1);
     expect(wrapper.find('.card-header')).toHaveLength(1);
+    wrapper.find('.card').at(0).simulate('click');
+    expect(onClick).toBeCalledTimes(1);
     wrapper.find('.card-close').at(0).simulate('click');
     expect(onClose).toBeCalledTimes(1);
   });
