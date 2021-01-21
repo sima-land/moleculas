312b837da7d34434ce0b09428f1035ce06afe658
дима петров <petrov_dm@ekb.sima-land.ru>
2021-01-21 09:38:58 +0500

    feat(Badge): Добавлена базовая версия компонента Badge для вывода шильдиков.

---
+++ src/common/components/badge/__stories__/badge.stories.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import { Badge } from '..';
+import { addDays, addMonths } from 'date-fns';
+
+export default {
+  title: 'common/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'padded',
+    docs: {
+      description: {
+        component: 'Компонент шильдика',
+      },
+    },
+  },
+};
+
+export const Primary = props => <Badge {...props} />;
+
+Primary.storyName = 'Использование';
+Primary.args = {
+  href: 'https://sima-land.ru',
+  color: '#002b41',
+  fields: [
+    { type: 'text', value: 'Товар партнёра' },
+  ],
+};
+
+export const TextView = () => (
+  <Badge
+    color='#00b8d4'
+    href='https://sima-land.ru'
+    fields={[
+      { type: 'text', value: 'Система скидок' },
+    ]}
+  />
+);
+
+TextView.storyName = 'Текст';
+TextView.parameters = {
+  docs: {
+    description: {
+      story: 'Вариант, содержащий только текст',
+    },
+  },
+};
+
+export const IconView = () => (
+  <Badge
+    color='#ff7200'
+    href='https://sima-land.ru'
+    fields={[
+      { type: 'svg-url', value: 'https://static2.static1-sima-land.com/image/mobile_app/common/notice_icon.svg' },
+    ]}
+  />
+);
+
+IconView.storyName = 'Иконка';
+IconView.parameters = {
+  docs: {
+    description: {
+      story: 'Вариант, содержащий только конку',
+    },
+  },
+};
+
+export const TimerView = () => (
+  <Badge
+    color='#b52ea8'
+    href='https://sima-land.ru'
+    fields={[
+      { type: 'timer', value: addDays(new Date(), 7).toISOString() },
+    ]}
+  />
+);
+
+TimerView.storyName = 'Таймер';
+TimerView.parameters = {
+  docs: {
+    description: {
+      story: 'Вариант, содержащий только таймер',
+    },
+  },
+};
+
+export const TimerTextView = () => (
+  <>
+    <Badge
+      color='#b52ea8'
+      href='https://sima-land.ru'
+      fields={[
+        { type: 'timer', value: addMonths(new Date(), 3).toISOString() },
+        { type: 'text', value: ' — Специальные цены на музыкальные инструменты' },
+      ]}
+    />
+    <div style={{ height: 8 }} />
+    <Badge
+      color='#b52ea8'
+      href='https://sima-land.ru'
+      fields={[
+        { type: 'text', value: '-45%' },
+        { type: 'timer', value: addMonths(new Date(), 3).toISOString() },
+      ]}
+    />
+  </>
+);
+
+TimerTextView.storyName = 'Таймер + текст';
+TimerTextView.parameters = {
+  docs: {
+    description: {
+      story: 'Вариант, содержащий таймер и текст',
+    },
+  },
+};
+
+export const OverflowView = () => (
+  <div style={{ maxWidth: 240 }}>
+    <Badge
+      color='#b52ea8'
+      href='https://sima-land.ru'
+      fields={[
+        { type: 'text', value: '-56% — Специальные цены на товары для росписи' },
+      ]}
+    />
+  </div>
+);
+
+OverflowView.storyName = 'Переполнение';
+OverflowView.parameters = {
+  docs: {
+    description: {
+      story: 'Вариант с ограничением ширины контейнера',
+    },
+  },
+};
+
+export const NoInteractiveView = () => (
+  <Badge
+    color='#607d8b'
+    fields={[
+      { type: 'text', value: 'Уценённый товар' },
+    ]}
+  />
+);
+
+NoInteractiveView.storyName = 'Без ссылки и hover';
+NoInteractiveView.parameters = {
+  docs: {
+    description: {
+      story: 'Вариант без ссылки: отключается эффект при наведении',
+    },
+  },
+};
---
+++ src/common/components/badge/index.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import { ReactSVG } from 'react-svg';
+import Timer from '@dev-dep/ui-nucleons/timer';
+import styles from './badge.scss';
+
+const cx = classnames.bind(styles);
+
+/**
+ * Шильдик.
+ * @param {Object} props Свойства.
+ * @param {string} [props.className] Класс.
+ * @param {string} props.color Цвет.
+ * @param {Array<{ type: 'text' | 'timer' | 'svg-url', value: string }>} props.fields Содержимое.
+ * @param {string} props.href Ссылка.
+ * @param {Object} [props.style] Стили.
+ * @return {ReactElement} Компонент.
+ */
+export const Badge = ({
+  className,
+  color,
+  fields,
+  href,
+  style,
+  ...restProps
+}) => {
+  const Element = href ? 'a' : 'span';
+  const iconOnly = fields.length === 1 && fields[0].type === 'svg-url';
+
+  return (
+    <Element
+      {...restProps}
+      href={href}
+      style={{ ...style, '--badge-color': color }}
+      className={cx(
+        'root',
+        iconOnly && 'icon-only',
+        href && 'interactive',
+        className
+      )}
+      children={
+        iconOnly
+          ? (
+            <ReactSVG
+              wrapper='span'
+              className={cx('svg-wrapper')}
+              src={fields[0].value}
+            />
+          ) : (
+
+            // вложенный span нужен для того чтобы объединить `display: inline-flex` и `text-overflow: ellipsis`
+            <span className={cx('content')}>
+              {fields.reduce((acc, item, i) => {
+                let result = null;
+
+                // на данный момент сочетание иконки с другими элементами не предусмотрено макетами - игнорируем
+                switch (item.type) {
+                  case 'text':
+                    result = item.value;
+                    break;
+                  case 'timer':
+                    result = (
+                      <Timer
+                        key={i}
+                        date={item.value}
+                        timeout={1000 * 60}
+                        format={formatDistance}
+                      />
+                    );
+                    break;
+                }
+
+                acc.push(result);
+                i < fields.length - 1 && acc.push(' ');
+
+                return acc;
+              }, [])}
+            </span>
+          )
+      }
+    />
+  );
+};
+
+/**
+ * Форматирует оставшееся время.
+ * @param {{ days, hours, minutes }} distance Оставшееся время.
+ * @return {string} Отформатированное время.
+ */
+const formatDistance = ({ days, hours, minutes }) => [
+  toTimePart(days),
+  ...[hours % 24, minutes % 60].map(toTimePart),
+].join(':');
+
+/**
+ * Форматирует число добавляя нули при необходимости.
+ * @param {number} n Число.
+ * @return {string} Отформатированное число.
+ */
+const toTimePart = n => `${n}`.padStart(2, '0');
