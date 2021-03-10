0dc9a417b2d055268bebc8f2081c2032ab833212
pakvakun <zhitnikov.1996@gmail.com>
2021-03-10 16:16:38 +0500

    #75 Добавил в библиотеку компонент ProductCard

---
+++ src/mobile/components/product-card/__test__/index.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ProductCard from '..';
+import noop from 'lodash/noop';
+
+describe('ProductCard', () => {
+  const item = {
+    itemUrl: 'test',
+    imageUrl: 'https://cdn3.static1-sima-land.com/items/29455/1/140.jpg?v=1598850721',
+    name: 'Зеленый слоник 25мм МИКС',
+    sid: 123,
+    count: 23,
+    initialCount: 42,
+    currencyGrapheme: '₽',
+    commonPrice: 50,
+    itemPrice: 5,
+    onWishButtonClick: noop,
+    onActionsClick: noop,
+    isFetchingWishItems: noop,
+    notInStock: true,
+    isWished: true,
+    movedOrderId: 123456,
+    deliveryCost: 10,
+    flags: [],
+  };
+  it('renders properly', () => {
+    const wrapper = mount(<ProductCard {...item} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
