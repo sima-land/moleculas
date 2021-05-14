d92197c53750de9043880082f5a4e03d1d64c815
Dmitriy Zhitnikov <zhitnikov.1996@gmail.com>
2021-05-14 06:10:12 +0000

    #85 Доработано модальное окно отслеживания в десктопной версии

--- src/mobile/components/tracking-info-screen/__stories__/tracking-info-screen.stories.jsx
+++ src/mobile/components/tracking-info-screen/__stories__/tracking-info-screen.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TrackingInfoScreen } from '../';
 
 export default {
-  title: 'mobile/Tracking-info-screen',
+  title: 'mobile/TrackingInfoScreen',
   component: TrackingInfoScreen,
   parameters: {
     layout: 'padded',
@@ -24,8 +24,13 @@ export const Primary = Template.bind({});
 Primary.storyName = 'Экран отслеживания';
 Primary.args = {
   trackNumbers: [
-    { title: 'title', trackNumber: '1234567' },
-
+    { title: 'Трек-номер 1 посылки', trackNumber: '62096122576448448' },
+    { title: 'Трек-номер 2 посылки', trackNumber: '62096122576448448' },
+    { title: 'Трек-номер 3 посылки', trackNumber: '62096122576448448' },
+    { title: 'Трек-номер 4 посылки', trackNumber: '62096122576448448' },
+    { title: 'Трек-номер 5 посылки', trackNumber: '62096122576448448' },
+    { title: 'Трек-номер 6 посылки', trackNumber: '62096122576448448' },
+    { title: 'Трек-номер 7 посылки', trackNumber: '62096122576448448' },
   ],
   deliverySite: 'cdek.ru',
 };
