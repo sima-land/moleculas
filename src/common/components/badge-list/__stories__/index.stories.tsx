991bc8274c11f2bc600d56d85bf2c53137e94475
krutoo <dima.krutoo@yandex.ru>
2021-09-10 16:53:23 +0500

    #31
    - FlagsList переделан в BadgeList

---
+++ src/common/components/badge-list/__stories__/index.stories.tsx
@@ -0,0 +1,77 @@
+import { addMonths } from 'date-fns';
+import React from 'react';
+import { BadgeList } from '..';
+import { BadgeProps } from '../../badge';
+
+const items: BadgeProps[] = [
+  {
+    href: 'https://sima-land.ru',
+    color: '#002b41',
+    fields: [
+      { type: 'text', value: 'Товар партнёра' },
+    ],
+  },
+  {
+    color: '#00b8d4',
+    href: 'https://sima-land.ru',
+    fields: [
+      { type: 'text', value: 'Система скидок' },
+    ],
+  },
+  {
+    color: '#b52ea8',
+    href: 'https://sima-land.ru',
+    fields: [
+      { type: 'timer', value: addMonths(new Date(), 3).toISOString() },
+      { type: 'text', value: ' — Специальные цены на музыкальные инструменты' },
+    ],
+  },
+];
+
+export default {
+  title: 'common/BadgeList',
+  component: BadgeList,
+  parameters: {
+    layout: 'padded',
+    docs: {
+      description: {
+        component: 'Компонент списка шильдиков',
+      },
+    },
+  },
+};
+
+const DemoBlock = ({ children }: { children: React.ReactNode }) => (
+  <div style={{ display: 'flex' }}>
+    <div style={{ border: '20px solid #ddd', marginBottom: '16px' }}>
+      <div style={{ maxWidth: '196px' }}>
+        {children}
+      </div>
+    </div>
+  </div>
+);
+
+export const Primary = () => (
+  <>
+    <DemoBlock>
+      <BadgeList
+        lineLimit={3}
+        items={items}
+      />
+    </DemoBlock>
+
+    <DemoBlock>
+      <BadgeList
+        lineLimit={2}
+        items={items}
+      />
+    </DemoBlock>
+
+    <DemoBlock>
+      <BadgeList
+        lineLimit={1}
+        items={items}
+      />
+    </DemoBlock>
+  </>
+);
---
+++ src/common/components/badge-list/__test__/index.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { BadgeList } from '../index';
+import { Badge, BadgeProps } from '../../badge';
+
+describe('<BadgeList />', () => {
+  describe('without props', () => {
+    const wrapper = mount(<BadgeList />);
+
+    it('renders correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('with props', () => {
+    const items: BadgeProps[] = [
+      {
+        href: '/sale/?is_label=1',
+        color: '#64a0eb',
+        fields: [
+          {
+            type: 'text',
+            value: '-10%',
+          },
+        ],
+      },
+    ];
+    const wrapper = mount(<BadgeList items={items} className='test' />);
+
+    it('renders correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('contains flags', () => {
+      const $flag = wrapper.find(Badge);
+      expect($flag).toHaveLength(1);
+      expect($flag.prop('fields')).toEqual([{
+        type: 'text',
+        value: '-10%',
+      }]);
+    });
+  });
+});
