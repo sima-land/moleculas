a4c1c13d214bdce5198806a638c6cb56c1c25fb9
krutoo <dima.krutoo@yandex.ru>
2021-10-27 14:20:49 +0500

    #65
    - добавлен пакет темизации storybook
    - storybook: добавлен вывод кастомного логотипа

---
+++ .storybook/manager.js
@@ -0,0 +1,12 @@
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming';
+import brandImage from './logo.png';
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'Дизайн-система Сима-ленд',
+    brandUrl: 'https://www.sima-land.ru/',
+    brandImage,
+  }),
+});
