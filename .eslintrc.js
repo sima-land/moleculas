2f5f3d2dd28366570311e5afa8cd167c15ca2f0c
krutoo <dima.krutoo@yandex.ru>
2021-10-28 11:46:15 +0500

    #51
    - обновлен пакет линтеров
    - установлены eslint, stylelint, prettier
    - добавлены конфиг и скрипты prettier
    - правки скрипта `lint:styles`
    - правки конфигов eslint
    - добавлен прогон prettier (в режиме проверки) в CI
    - правки конфига lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
   extends: require.resolve('@sima-land/linters/eslint'),
   overrides: [
@@ -14,60 +16,6 @@ module.exports = {
         'require-jsdoc': 'off',
         'react/prop-types': 'off',
         'import/no-anonymous-default-export': 'off',
-      },
-    },
-    {
-      files: [
-        '*.ts',
-        '*.tsx',
-      ],
-      plugins: [
-        '@typescript-eslint',
-      ],
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-      ],
-      rules: {
-        semi: 'off',
-        '@typescript-eslint/semi': ['error'],
-        indent: 'off',
-        '@typescript-eslint/indent': [
-          'error',
-          2,
-          {
-            SwitchCase: 1,
-            MemberExpression: 1,
-            ObjectExpression: 1,
-            ignoredNodes: [
-              'TemplateLiteral *',
-            ],
-          },
-        ],
-        '@typescript-eslint/no-explicit-any': 'off',
-        'no-extra-parens': 'off',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': 'error',
-        '@typescript-eslint/no-extra-parens': [
-          'error',
-          'all',
-          {
-            ignoreJSX: 'all',
-            nestedBinaryExpressions: false,
-          },
-        ],
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-use-before-define': ['error', {
-          variables: false,
-          functions: false,
-          classes: false,
-        }],
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        'jsdoc/require-param-type': 'off',
-        'jsdoc/require-returns-type': 'off',
-        'jsdoc/require-yields': 'off',
       },
     },
   ],
---
+++ .prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@sima-land/linters/prettier');
