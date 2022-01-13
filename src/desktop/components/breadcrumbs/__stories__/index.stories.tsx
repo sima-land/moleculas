c8413ddae07bef33614a5db0c572d1540390eda7
krutoo <dima.krutoo@yandex.ru>
2022-01-13 12:56:19 +0500

    #66
    - desktop/Breadcrumbs: тестовые данные перенесены в папку тестов (patch)

--- src/desktop/components/breadcrumbs/__stories__/index.stories.tsx
+++ src/desktop/components/breadcrumbs/__stories__/index.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Breadcrumbs } from '..';
-import items from '../items';
+import items from '../__test__/test-items';
 
 export default {
   title: 'desktop/Breadcrumbs',
