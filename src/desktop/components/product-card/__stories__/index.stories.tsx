5fea9273a81089cac649e942c7590ae537ea818d
krutoo <dima.krutoo@yandex.ru>
2022-01-14 11:44:02 +0500

    #66
    - ProductCard: переделан, теперь является карточкой со всплыващей плашкой при наведении (major)

--- src/desktop/components/product-card/__stories__/index.stories.tsx
+++ src/desktop/components/product-card/__stories__/index.stories.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { ProductCard } from '..';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@sima-land/ui-nucleons/button';
+import { ProductCard } from '..';
+import { ProductInfo, Parts } from '../../../../common/components/product-info';
 import { Stepper } from '@sima-land/ui-nucleons/stepper';
-import { ProductData } from '../../../../common/components/product-info';
+import { COLORS } from '@sima-land/ui-nucleons/colors';
+import FavSVG from '@sima-land/ui-quarks/icons/24x24/Filled/favorite';
+import NotFavSVG from '@sima-land/ui-quarks/icons/24x24/Stroked/favorite';
+import { Badge, BadgeProps } from '../../../../common/components/badge';
 
 export default {
   title: 'desktop/ProductCard',
@@ -18,92 +21,168 @@ export default {
   },
 };
 
-const product: ProductData = {
-  name: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis, nulla.',
-  url: 'https://sima-land.ru',
-  price: 1200000,
-  oldPrice: 1450000,
-  currencyGrapheme: '₽',
-  imageSrc: 'https://picsum.photos/240',
-  badges: [
-    { color: '#b52ea8', fields: [{ type: 'text', value: '-56%' }] },
-    { color: '#ff7200', fields: [{ type: 'text', value: '3 по цене 2' }] },
-    { color: '#2962ff', fields: [{ type: 'text', value: 'Товар месяца' }] },
-    { color: '#00b8d4', fields: [{ type: 'text', value: 'Акция' }] },
-  ],
-  trademark: {
-    name: 'ART UZOR Predictable trademark name here!',
-    url: 'https://sima-land.ru',
-  },
-};
-
-const variants: Array<React.CSSProperties> = [
-  { width: '240px' },
-  { width: '180px', marginLeft: '32px' },
+const badges: BadgeProps[] = [
+  { color: '#b52ea8', fields: [{ type: 'text', value: '-56%' }] },
+  { color: '#ff7200', fields: [{ type: 'text', value: '3 по цене 2' }] },
+  { color: '#2962ff', fields: [{ type: 'text', value: 'Товар месяца' }] },
+  { color: '#00b8d4', fields: [{ type: 'text', value: 'Акция' }] },
 ];
 
+const data = {
+  name: 'Ножницы портновские, с прорезиненной ручкой, 20 см, цвет чёрный/серый',
+  imageSrc: 'https://picsum.photos/360',
+  url: 'https://www.sima-land.ru',
+  price: 99876543.21,
+  oldPrice: 99987654.32,
+  currencyGrapheme: '₽',
+  badges,
+  trademark: { name: 'Торговая марка Сима-ленд', url: 'https://www.sima-land.ru' },
+};
+
+const Bootstrap: React.FC = ({ children }) => (
+  <div style={{ width: '240px', margin: '20px' }}>{children}</div>
+);
+
 export const Primary = () => {
-  const [amount, setAmount] = useState<number>(0);
+  const [wished, toggleWish] = useState<boolean>(false);
 
   return (
-    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-      {variants.map((style, index) => (
-        <ProductCard
-          key={index}
-          style={style}
-          data={product}
-          onLinkClick={event => {
-            event.preventDefault();
-            action('product link click')();
-          }}
-          onQuickViewClick={() => {
-            action('quick view button click')();
-          }}
-        >
-          <ProductCard.CartControl
-            stepText={amount > 0 ? 'по 1 шт.' : 'от 2 шт.'}
-            markupText='Комплектация + 50 ₽ при покупке до 20 шт'
+    <Bootstrap>
+      <ProductCard>
+        <ProductInfo>
+          <Parts.Image
+            src={data.imageSrc}
+            href={data.url}
+            onClick={e => {
+              e.preventDefault();
+              action('Клик: ссылка на товар (изображение)')();
+            }}
           >
-            {amount > 0 ? (
-              <Stepper
-                size='s'
-                value={amount}
-                onAdd={() => setAmount(amount + 1)}
-                onSubtract={() => setAmount(amount - 1)}
-              />
-            ) : (
-              <Button
-                size='s'
-                style={{ width: '122px' }}
-                onClick={() => setAmount(Math.max(2, amount + 1))}
-              >
-                В корзину
-              </Button>
-            )}
-          </ProductCard.CartControl>
-        </ProductCard>
-      ))}
-    </div>
+            <Parts.ImageButton
+              icon={wished ? FavSVG : NotFavSVG}
+              hint={wished ? 'Убрать из избранного' : 'Добавить в избранное'}
+              fill={wished ? COLORS.get('additional-red') : undefined}
+              onClick={() => {
+                toggleWish(a => !a);
+                action('Клик: добавить в избранное')();
+              }}
+              data-testid='favorite-button'
+            />
+          </Parts.Image>
+
+          {data.badges && (
+            <Parts.Badges lineLimit={2}>
+              {data.badges.map((badge, badgeIndex) => (
+                <Badge key={badgeIndex} {...badge} onClick={action('Клик: шильдик')} />
+              ))}
+            </Parts.Badges>
+          )}
+
+          <Parts.Prices
+            price={data.price}
+            oldPrice={data.oldPrice}
+            currencyGrapheme={data.currencyGrapheme}
+          />
+
+          <Parts.Title
+            href={data.url}
+            onClick={e => {
+              e.preventDefault();
+              action('Клик: ссылка на товар')();
+            }}
+          >
+            {data.name}
+          </Parts.Title>
+
+          <Parts.TrademarkLink
+            href={data.trademark.url}
+            onClick={e => {
+              e.preventDefault();
+              action('Клик: ссылка на торговую марку')();
+            }}
+          >
+            {data.trademark.name}
+          </Parts.TrademarkLink>
+
+          <Parts.Footer>
+            <Parts.CartControl
+              stepText='По 5 шт'
+              markupText='Комплектация + 50 ₽ при покупке до 20 шт'
+            >
+              <Stepper defaultValue={12} size='s' style={{ width: '122px' }} />
+            </Parts.CartControl>
+          </Parts.Footer>
+        </ProductInfo>
+      </ProductCard>
+    </Bootstrap>
   );
 };
 
-export const CartLoading = () => (
-  <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-    {variants.map((style, index) => (
-      <ProductCard
-        key={index}
-        style={style}
-        data={product}
-        onLinkClick={event => {
-          event.preventDefault();
-          action('product link click')();
-        }}
-        onQuickViewClick={() => {
-          action('quick view button click')();
-        }}
-      >
-        <ProductCard.CartControl loading />
+export const NotEnough = () => {
+  const [wished, toggleWish] = useState<boolean>(false);
+
+  return (
+    <Bootstrap>
+      <ProductCard>
+        <ProductInfo restriction='unavailable'>
+          <Parts.Image
+            src={data.imageSrc}
+            href={data.url}
+            onClick={e => {
+              e.preventDefault();
+              action('Клик: ссылка на товар (изображение)')();
+            }}
+          >
+            <Parts.ImageButton
+              icon={wished ? FavSVG : NotFavSVG}
+              hint={wished ? 'Убрать из избранного' : 'Добавить в избранное'}
+              fill={wished ? COLORS.get('additional-red') : undefined}
+              onClick={() => {
+                toggleWish(a => !a);
+                action('Клик: добавить в избранное')();
+              }}
+              data-testid='favorite-button'
+            />
+          </Parts.Image>
+
+          <Parts.Badges lineLimit={1}>
+            {data.badges.map((badge, index) => (
+              <Badge key={index} {...badge} onClick={action('Клик: шильдик')} />
+            ))}
+          </Parts.Badges>
+
+          <Parts.Prices
+            price={data.price}
+            oldPrice={data.oldPrice}
+            currencyGrapheme={data.currencyGrapheme}
+            unavailableReason='Товара нет в наличии'
+          />
+
+          <Parts.Title
+            href={data.url}
+            onClick={e => {
+              e.preventDefault();
+              action('Клик: ссылка на товар')();
+            }}
+          >
+            {data.name}
+          </Parts.Title>
+
+          <Parts.TrademarkLink
+            href={data.trademark.url}
+            onClick={e => {
+              e.preventDefault();
+              action('Клик: ссылка на торговую марку')();
+            }}
+          >
+            {data.trademark.name}
+          </Parts.TrademarkLink>
+
+          <Parts.Footer>
+            <Parts.WaitListAddButton />
+          </Parts.Footer>
+        </ProductInfo>
       </ProductCard>
-    ))}
-  </div>
-);
+    </Bootstrap>
+  );
+};
