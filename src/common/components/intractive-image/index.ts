e55f9690bc95bc5483f4d36455f52ecc318192d1
krutoo <dima.krutoo@yandex.ru>
2022-12-28 14:59:46 +0500

    #127
    - interactive-image: вернулись экспорты типов в точке входа (patch)
    - interactive-image: добавлен экспорт типа InteractiveImageStyle

---
+++ src/common/components/intractive-image/index.ts
@@ -0,0 +1,16 @@
+import type {
+  InteractiveImageStyle,
+  InteractiveImageProps,
+  InteractiveImageImageProps,
+  InteractiveImagePointProps,
+} from './types';
+import { InteractiveImage, Parts } from './interactive-image';
+
+export type {
+  InteractiveImageStyle,
+  InteractiveImageProps,
+  InteractiveImageImageProps,
+  InteractiveImagePointProps,
+};
+
+export { InteractiveImage, Parts };
