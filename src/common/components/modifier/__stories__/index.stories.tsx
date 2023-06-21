97bd6aee9f4ff3178b5032dca6ba4b97d6c4ccfb
krutoo <dima.krutoo@yandex.ru>
2023-06-21 12:41:11 +0500

    #170
    - modifier: добавлена сторя с тестом битых картинок

--- src/common/components/modifier/__stories__/index.stories.tsx
+++ src/common/components/modifier/__stories__/index.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modifier, ModifierContent, MoreButton } from '..';
 import { Expandable } from '@sima-land/ui-nucleons/expandable';
 import { Layout } from '@sima-land/ui-nucleons/layout';
@@ -178,3 +178,27 @@ export function WithCarousel() {
 }
 
 WithCarousel.storyName = 'Вместе с Carousel';
+
+export function TestImageBroken() {
+  const [broken, setBroken] = useState(false);
+  const src = broken ? 'http://non-existed-site.com/' : 'https://loremflickr.com/240/240';
+
+  return (
+    <>
+      <button onClick={() => setBroken(a => !a)}>{broken ? 'Починить' : 'Сломать'}</button>
+
+      <div style={{ display: 'flex', gap: 12, alignItems: 'flex-start', marginTop: 12 }}>
+        <Modifier size='s' content={{ type: 'image', src }} />
+        <Modifier size='m' content={{ type: 'image', src }} />
+
+        <Modifier size='s' active content={{ type: 'image', src }} />
+        <Modifier size='m' active content={{ type: 'image', src }} />
+
+        <Modifier size='s' disabled content={{ type: 'image', src }} />
+        <Modifier size='m' disabled content={{ type: 'image', src }} />
+      </div>
+    </>
+  );
+}
+
+TestImageBroken.storyName = 'Тест: ошибка загрузки картинки';
