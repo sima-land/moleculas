144d0af9e671f59c46e6cb205d72c39e0e2d0c9a
Dmitry Petrov <petrov_dm@ekb.sima-land.ru>
2023-09-08 19:58:07 +0500

    #197
    - удалены тесты на enzyme

--- src/desktop/components/gallery-modal/components/__test__/thumbnail.test.tsx
+++ src/desktop/components/gallery-modal/components/__test__/thumbnail.test.tsx
@@ -1,61 +1,43 @@
-import { mount } from 'enzyme';
+import { fireEvent, render } from '@testing-library/react';
 import { Thumbnail } from '../thumbnail';
-import { act } from 'react-dom/test-utils';
 
 describe('Thumbnail', () => {
   it('should render 360 icon', () => {
     const spy = jest.fn();
 
-    const wrapper = mount(<Thumbnail type='360' onClick={spy} />);
-
-    expect(wrapper).toMatchSnapshot();
+    const { container, getByRole } = render(<Thumbnail type='360' onClick={spy} />);
 
     expect(spy).toHaveBeenCalledTimes(0);
+    expect(container.querySelectorAll('svg')).toHaveLength(1);
 
-    act(() => {
-      wrapper.find('button').simulate('click');
-    });
-
+    fireEvent.click(getByRole('button'));
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should render video icon', () => {
     const spy = jest.fn();
 
-    const wrapper = mount(<Thumbnail type='video' onClick={spy} />);
-
-    expect(wrapper).toMatchSnapshot();
+    const { container, getByRole } = render(<Thumbnail type='video' onClick={spy} />);
 
     expect(spy).toHaveBeenCalledTimes(0);
+    expect(container.querySelectorAll('svg')).toHaveLength(1);
 
-    act(() => {
-      wrapper.find('button').simulate('click');
-    });
-
+    fireEvent.click(getByRole('button'));
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should render image', () => {
     const spy = jest.fn();
 
-    const wrapper = mount(<Thumbnail type='image' src='https://image.com/1' onClick={spy} />);
-
-    expect(wrapper).toMatchSnapshot();
+    const { container, getByRole } = render(
+      <Thumbnail type='image' src='https://image.com/1' onClick={spy} />,
+    );
 
     expect(spy).toHaveBeenCalledTimes(0);
+    expect(container.querySelectorAll('img')).toHaveLength(1);
 
-    act(() => {
-      wrapper.find('button').simulate('click');
-    });
+    fireEvent.click(getByRole('button'));
 
     expect(spy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should render 360 checked icon', () => {
-    const spy = jest.fn();
-
-    const wrapper = mount(<Thumbnail checked type='360' onClick={spy} />);
-
-    expect(wrapper).toMatchSnapshot();
   });
 });
