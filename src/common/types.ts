18ca7277c4ef3e46ddfe2288a06d7dacc8d0303d
Dmitry Petrov <dima.krutoo@yandex.ru>
2024-01-09 16:36:46 +0500

    #213 ProductCard: добавить возможность определять фильтры содержимого (#214)
    - common: удалены типы и константы устаревших модификаторов (major)
    - product-card: добавлены пропсы reduceBaseInfo и reduceHoverInfo (minor)

--- src/common/types.ts
+++
@@ -1,1 +0,0 @@
-export type ModifierType = 'text' | 'image' | 'color';
---
+++ src/desktop/components/product-card/product-card.tsx
@@ -0,0 +1,128 @@
+import { Children, forwardRef, isValidElement, cloneElement, useState } from 'react';
+import { ProductCardChildren, ProductCardProps } from './types';
+import { Parts } from '../../../common/components/product-info';
+import { Plate, PlateProps } from '@sima-land/ui-nucleons/plate';
+import { useLayer } from '@sima-land/ui-nucleons/helpers/layer';
+import classNames from 'classnames/bind';
+import styles from './product-card.module.scss';
+
+const cx = classNames.bind(styles);
+
+/**
+ * Карточка данных товара с расширенной инф-ой при наведении.
+ * @param props Свойства.
+ * @return Элемент.
+ */
+export function ProductCard({
+  reduceBaseInfo: reduceBaseInfoProp = reduceBaseInfo,
+  reduceHoverInfo: reduceHoverInfoProp = reduceHoverInfo,
+  children,
+  className,
+  ...props
+}: ProductCardProps) {
+  const [hovered, toggle] = useState<boolean>(false);
+
+  return (
+    <div
+      {...props}
+      data-testid='product-card'
+      className={cx('root', className)}
+      onMouseEnter={() => toggle(true)}
+      // ВАЖНО: вешаем onMouseLeave на корневой элемент так как в Chrome проблемы если вешать на "product-card:info"
+      onMouseLeave={() => toggle(false)}
+    >
+      <div data-testid='product-card:info'>{reduceBaseInfoProp(children)}</div>
+
+      {hovered && <HoverCard>{reduceHoverInfoProp(children)}</HoverCard>}
+    </div>
+  );
+}
+
+export const HoverCard = forwardRef<HTMLDivElement | null, PlateProps>((props, ref) => {
+  const layer = useLayer() + 1;
+
+  return (
+    <Plate
+      {...props}
+      style={{ ...props.style, zIndex: layer }}
+      ref={ref}
+      rounds='m'
+      shadow='z4'
+      className={cx('card', props.className)}
+      data-testid='product-card:hover-card'
+    />
+  );
+});
+
+/**
+ * Фильтрует содержимое карточки для вывода состояния без наведения.
+ * @param element Элемент.
+ * @param options Опции.
+ * @return Элемент.
+ */
+export function reduceBaseInfo(
+  element: ProductCardChildren,
+  {
+    hideImageButtons = true,
+    hideFooter = true,
+  }: {
+    hideImageButtons?: boolean;
+    hideFooter?: boolean;
+  } = {},
+) {
+  return cloneElement(element, {
+    children: Children.map(element.props.children, child => {
+      let result;
+
+      if (isValidElement(child)) {
+        switch (true) {
+          // игнорируем кнопки-иконки на изображении
+          case Boolean(child.type === Parts.Image && hideImageButtons):
+            result = cloneElement<any>(child, { children: undefined });
+            break;
+
+          // игнорируем футер
+          case Boolean(child.type === Parts.Footer && hideFooter):
+            result = null;
+            break;
+
+          // остальное оставляем без изменений
+          default:
+            result = child;
+            break;
+        }
+      }
+
+      return result;
+    }),
+  });
+}
+
+/**
+ * Получив содержимое карточки фильтрует/меняет его для вывода в карточке при наведении.
+ * @param element Содержимое.
+ * @return Элемент.
+ */
+export function reduceHoverInfo(element: ProductCardChildren) {
+  return cloneElement(element, {
+    children: Children.map(element.props.children, child => {
+      let result;
+
+      if (isValidElement(child)) {
+        switch (child.type) {
+          // при наведении у изображения строго непрозрачность = 1
+          case Parts.Image:
+            result = cloneElement<any>(child, { opacity: 1 });
+            break;
+
+          // остальное оставляем без изменений
+          default:
+            result = child;
+            break;
+        }
+      }
+
+      return result;
+    }),
+  });
+}
